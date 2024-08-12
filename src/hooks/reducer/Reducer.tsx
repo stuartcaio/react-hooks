72bf4a1de971f66cc58348e0635aeb126750b125
caio.stuart <caio.stuart@gruppe.com.br>
2024-08-12 11:22:16 -0300

    Adding useReducer.

--- src/hooks/reducer/Reducer.tsx
+++ src/hooks/reducer/Reducer.tsx
@@ -1,7 +1,30 @@
+import { useReducer } from "react"
+
+type actions = {
+    type: "INCREMENT" | "DECREMENT"
+};
+
+function reducer(state: {count: number}, action: actions){
+    if(action.type === "INCREMENT"){
+        return {...state, count: state.count + 1};
+    } else if(action.type === "DECREMENT"){
+        return {...state, count: state.count - 1};
+    } else{
+        return state.count;
+    }
+}
+
 export default function Reducer(){
+    const [state, dispatch] = useReducer(reducer, {count: 0});   
+
     return (
         <div>
-            Reducer
+            <h1>Reducer</h1>
+            <p>Value: {state.count}</p>
+            <div>
+                <button onClick={() => dispatch({type: "INCREMENT"})}>Increment</button>
+                <button onClick={() => dispatch({type: "DECREMENT"})}>Decrement</button>
+            </div>
         </div>
     )
 }
