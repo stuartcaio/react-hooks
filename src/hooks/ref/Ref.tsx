3f8cd42cdcc0107a089fe890e5aab7fd18209eca
caio.stuart <caio.stuart@gruppe.com.br>
2024-08-12 11:13:42 -0300

    Adding useRef.

--- src/hooks/ref/Ref.tsx
+++ src/hooks/ref/Ref.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useRef, useState } from "react"
+
 export default function Ref(){
+    const paragraph = useRef();
+    const [showText, setShowText] = useState(false);
+
+    useEffect(() => {
+        if(showText){
+            paragraph.current.innerHTML = "ipsom Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum et magni temporibus minima doloremque cumque, modi earum veritatis";
+        } else{
+            paragraph.current.innerHTML = "";
+        }
+    }, [showText]);
+
     return (
         <div>
-            Ref
+            <p ref={paragraph}></p>
+
+            <button onClick={() => setShowText(!showText)}>{showText ? "Hide" : "Show"} text</button>
         </div>
     )
 }
