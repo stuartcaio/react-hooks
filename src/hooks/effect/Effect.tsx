ab8683eee4d64d16fa0971320d2b54057aedf59c
caio.stuart <caio.stuart@gruppe.com.br>
2024-08-12 10:26:37 -0300

    Adding use effect.

--- src/hooks/effect/Effect.tsx
+++ src/hooks/effect/Effect.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+
 export default function Effect(){
+    const [value, setValue] = useState(0);
+    const [factorialValue, setFactorialValue] = useState(0);
+
+    useEffect(() => {
+        let currentFactorialCalculated = 0;
+
+        for(let i = 0; i < value; i++){
+            if(currentFactorialCalculated === 0){
+                currentFactorialCalculated = 1;
+            } else{
+                currentFactorialCalculated = currentFactorialCalculated * (i + 1);
+            }
+        }
+
+        setFactorialValue(currentFactorialCalculated);
+    }, [value]);
+
     return (
-        <div>
-            Effect
+        <div style={{display: "flex", flexDirection: "column", gap: "0.5rem", width: "20%", padding: "1rem"}}>
+            <h3>Value: {value}</h3>
+            <h3>Doubled value: {factorialValue}</h3>
+            <button onClick={() => setValue(value + 1)}>Add Value</button>
         </div>
     )
 }
