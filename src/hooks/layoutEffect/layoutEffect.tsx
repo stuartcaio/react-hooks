84d87f61c6b9a1277f3c4bbe89cf767b7f0fe4d6
caio.stuart <caio.stuart@gruppe.com.br>
2024-08-12 11:03:10 -0300

    Adding useLayoutEffect.

--- src/hooks/layoutEffect/layoutEffect.tsx
+++ src/hooks/layoutEffect/layoutEffect.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useLayoutEffect, useRef } from "react"
+
 export default function LayoutEffect(){
+    const paragraph = useRef();
+
+    useEffect(() => {
+        paragraph.current.innerHTML += "Caio";
+        console.log(`Use effect: ${paragraph.current.innerHTML}`);
+    }, []);
+
+    useLayoutEffect(() => {
+        console.log(`Layout effect: ${paragraph.current.innerHTML}`);
+    }, []);
+
     return (
-        <div>
-            Layout Effect
+        <div style={{display: "flex", flexDirection: "column", gap: "0.5rem", padding: "1rem"}}>
+            <h1>Layout Effect</h1>
+
+            <p ref={paragraph}>My name is </p>
         </div>
     )
 }
